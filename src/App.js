781d0991b37b1a033c20e8b2b965ef14d2973d79
goffjira <goffjira@gmail.com>
2020-09-17 18:01:40 +0700

    add responsive rules for the app container

--- src/App.js
+++ src/App.js
@@ -5,10 +5,8 @@ import Sidebar from "./Sidebar";
 function App() {
   return (
     <div className="app">
-      <h1>Let's bulid Twitter with React!!!</h1>
-
       <Sidebar />
-      {/* Feed */}
+      {/* <Feed /> */}
       {/* Widgets */}
     </div>
   );
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -15,7 +15,7 @@ import { Button } from "@material-ui/core";
 function Sidebar() {
   return (
     <div className="sidebar">
-      <TwitterIcon />
+      <TwitterIcon fontSize="large" className="sidebar__twitterIcon" />
       <SidebarOption text="Home" Icon={HomeOutlinedIcon} />
       <SidebarOption active text="Explore" Icon={SearchIcon} />
       <SidebarOption text="Notifications" Icon={NotificationsNoneIcon} />
