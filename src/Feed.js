0e3731e526eef9a2dc13908bebc2afe67f9928a2
goffjira <goffjira@gmail.com>
2020-09-18 13:55:49 +0700

    Add feed area with sample data

---
+++ src/Feed.js
@@ -0,0 +1,61 @@
+import React from "react";
+import "./Feed.css";
+import SettingsIcon from "@material-ui/icons/Settings";
+
+function Feed() {
+  return (
+    <div className="feed">
+      <div className="feed__head">
+        <h2>Home</h2>
+        <SettingsIcon fontSize="medium" />
+      </div>
+      <div>
+        Ea magna duis dolore irure labore veniam proident occaecat nostrud est
+        cupidatat laborum pariatur laboris. Elit sit tempor occaecat nostrud qui
+        magna quis veniam magna esse fugiat nisi dolore ad. Duis consectetur
+        duis dolore mollit aute ex. Sunt ea anim id deserunt duis. Amet minim
+        labore sint laboris excepteur esse et ex proident sit sunt. Do
+        exercitation cupidatat officia nisi aute occaecat ipsum eu dolore. Et
+        sit dolor duis deserunt commodo enim anim culpa irure. Est aliqua esse
+        aliqua qui tempor commodo occaecat. Aliquip enim aliqua cupidatat id
+        proident cillum. Officia ad sit mollit sunt culpa. Officia ea Lorem amet
+        ad. Cupidatat Lorem qui laborum aliquip cupidatat sit Lorem cillum. Non
+        exercitation id ipsum ad irure excepteur officia labore id irure est
+        cillum. Aute culpa nisi irure aliqua id labore commodo ullamco nulla
+        excepteur. Fugiat eiusmod aliquip et aute amet ipsum. Officia incididunt
+        est reprehenderit minim sint irure reprehenderit elit culpa cupidatat in
+        ex. Incididunt aute pariatur proident exercitation cillum do ut. Labore
+        occaecat id deserunt cillum esse sint non non. Proident pariatur anim ea
+        duis.
+      </div>
+      <h1>Cool</h1>
+      <h1>Cool</h1>
+      <h1>Cool</h1>
+      <h1>Cool</h1>
+      <h1>Cool</h1>
+      <h1>Cool</h1>
+      <h1>Cool</h1>
+      <h1>Cool</h1>
+      <h1>Cool</h1>
+      <h1>Cool</h1>
+      <h1>Cool</h1>
+      <h1>Cool</h1>
+      <h1>Cool</h1>
+      <h1>Cool</h1>
+      <h1>Cool</h1>
+      <h1>Cool</h1>
+      <h1>Cool</h1>
+      <h1>Cool</h1>
+      <h1>Cool</h1>
+      <h1>Cool</h1>
+      <h1>Cool</h1>
+      <h1>Cool</h1>
+      <h1>Cool</h1>
+      <h1>Cool</h1>
+      <h1>Cool</h1>
+      <h1>Cool</h1>
+    </div>
+  );
+}
+
+export default Feed;
