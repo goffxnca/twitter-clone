572e902816ac43bc5a9730d07ae88556be819f09
goffjira <goffjira@gmail.com>
2020-09-21 13:52:50 +0700

    Add initial unstyled trends widget

---
+++ src/Trend.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Trend.css";
+
+function Trend() {
+  return (
+    <div className="trend">
+      <div className="trend__country">Trending in Thailand</div>
+      <div className="trend__title">#TheDayOfJinYong</div>
+
+      <div className="trend__tweetsTotal">22.1K Tweets</div>
+    </div>
+  );
+}
+
+export default Trend;
--- src/Widgets.js
+++ src/Widgets.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Widgets.css";
 import SearchIcon from "@material-ui/icons/Search";
+import Trend from "./Trend";
 
 function Widgets() {
   return (
@@ -8,6 +9,14 @@ function Widgets() {
       <div className="widgets__search">
         <SearchIcon className="widgets__searchIcon" />
         <input type="text" placeholder="Search Twitter" spellCheck="false" />
+      </div>
+
+      <div className="widgets__trends">
+        <div className="widgets__trendHeader">
+          <span>Trends for you</span>
+        </div>
+        <Trend />
+        <Trend />
       </div>
     </div>
   );
