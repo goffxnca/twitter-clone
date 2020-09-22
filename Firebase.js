0f43833886f895da16b865b0c6ea1e68f9d49c66
goffjira <goffjira@gmail.com>
2020-09-22 12:24:03 +0700

    Install Firebase

---
+++ Firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDX__nlFlJLrkFkUc7jqlVjCoQo2n8SDWI",
+  authDomain: "twitter-clone-c135c.firebaseapp.com",
+  databaseURL: "https://twitter-clone-c135c.firebaseio.com",
+  projectId: "twitter-clone-c135c",
+  storageBucket: "twitter-clone-c135c.appspot.com",
+  messagingSenderId: "1013602434667",
+  appId: "1:1013602434667:web:ef67aa7305ac3bbb763b2e",
+  measurementId: "G-VC8GDYCZKT",
+};
