ad8a2c8c57130bf853537b1ece96bf86421ac82a
goffjira <goffjira@gmail.com>
2020-09-23 11:18:12 +0700

    Apply Checkout component to stick at bottom sidebar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -12,23 +12,31 @@ import PermIdentityIcon from "@material-ui/icons/PermIdentity";
 import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
 import PostAddIcon from "@material-ui/icons/PostAdd";
 import { Button } from "@material-ui/core";
+import Checkout from "./Checkout";
 
 function Sidebar() {
   return (
     <div className="sidebar">
-      <TwitterIcon fontSize="large" className="sidebar__twitterIcon" />
-      <SidebarOption active text="Home" Icon={HomeOutlinedIcon} />
-      <SidebarOption text="Explore" Icon={SearchIcon} />
-      <SidebarOption text="Notifications" Icon={NotificationsNoneIcon} />
-      <SidebarOption text="Messages" Icon={MailOutlineIcon} />
-      <SidebarOption text="Bookmarks" Icon={BookmarkBorderIcon} />
-      <SidebarOption text="Lists" Icon={ListAltIcon} />
-      <SidebarOption text="Profile" Icon={PermIdentityIcon} />
-      <SidebarOption text="More" Icon={MoreHorizIcon} />
-      <Button variant="outlined" className="baseTwitterButton  sidebar__tweet">
-        Tweet
-      </Button>
-      <PostAddIcon className="sidebar__PostAddIcon" />
+      <div class="sidebar__nav">
+        <TwitterIcon fontSize="large" className="sidebar__twitterIcon" />
+        <SidebarOption active text="Home" Icon={HomeOutlinedIcon} />
+        <SidebarOption text="Explore" Icon={SearchIcon} />
+        <SidebarOption text="Notifications" Icon={NotificationsNoneIcon} />
+        <SidebarOption text="Messages" Icon={MailOutlineIcon} />
+        <SidebarOption text="Bookmarks" Icon={BookmarkBorderIcon} />
+        <SidebarOption text="Lists" Icon={ListAltIcon} />
+        <SidebarOption text="Profile" Icon={PermIdentityIcon} />
+        <SidebarOption text="More" Icon={MoreHorizIcon} />
+        <Button
+          variant="outlined"
+          className="baseTwitterButton  sidebar__tweet"
+        >
+          Tweet
+        </Button>
+        <PostAddIcon className="sidebar__PostAddIcon" />
+      </div>
+
+      <Checkout class="sidebar__checkout" />
     </div>
   );
 }
